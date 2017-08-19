602f1fdff46166e5ea628bb945821a90054bd84e
iamDio <claudio.villani00@gmail.com>
2017-08-18 23:25:27 -0400

    set up react folder structure

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// Include the Main React Dependencies
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Include the Main Component
+//import from "./components/";
+
+// Render main component
+//ReactDOM.render(< />, document.getElementById(""));
