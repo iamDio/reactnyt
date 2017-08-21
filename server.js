8cc44c415ce6183e0af8048b4073e2e990239d90
iamDio <claudio.villani00@gmail.com>
2017-08-21 00:02:39 -0400

    added cheerio functionality

--- server.js
+++ server.js
@@ -3,9 +3,50 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var request = require("request");
 
 const app = express();
 const port = process.env.port || 3000;
+//-------------------------------------------------
+//set up cheerio
+
+// First, tell the console what server3.js is doing
+console.log("\n******************************************\n" +
+            "Look at the link of every article \n" +
+            "on the nyt.com website \n" +
+            "grab the url, title, and author." +
+            "\n******************************************\n");
+
+// Make request to grab the HTML from awwards's clean website section
+request("http://query.nytimes.com/", function(error, response, html) {
+
+  // Load the HTML into cheerio
+  var $ = cheerio.load(html);
+
+  // Make an empty array for saving our scraped info
+  var results = [];
+
+  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
+  $("figure.site").each(function(i, element) {
+
+    /* Cheerio's find method will "find" the first matching child element in a parent.
+     *    We start at the current element, then "find" its first child a-tag.
+     *    Then, we "find" the lone child img-tag in that a-tag.
+     *    Then, .attr grabs the imgs src value.
+     * So: <figure>  ->  <a>  ->  <img src="link">  ->  "link"  */
+    var imgLink = $(element).find("a").find("img").attr("src");
+
+    // Push the image's URL (saved to the imgLink var) into the results array
+    results.push({ link: imgLink });
+  });
+
+  // After looping through each element found, log the results to the console
+  console.log(results);
+});
+
+
+//-------------------------------------------------
 
 // Run Morgan for Logging
 app.use(logger("dev"));
